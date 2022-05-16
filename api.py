e94b80d323bd9a42401dfca841494aa81515122d
SantiPerez17 <55918957+SantiPerez17@users.noreply.github.com>
2022-05-16 10:27:58 -0300

    Update api.py

--- api.py
+++ api.py
@@ -4,8 +4,8 @@ from flask import Flask
 sonar.python.version=2.7, 3.7, 3.8, 3.9
 
 app = Flask(__name__)
-csrf = CSRFProtect()
-csrf.init_app(app)
+#csrf = CSRFProtect()
+#csrf.init_app(app)
 
 @app.route("/")
 def home():
