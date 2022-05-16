38858fe8b919034ae3a90cf13f4cfa0d892358c7
SantiPerez17 <55918957+SantiPerez17@users.noreply.github.com>
2022-05-16 10:18:39 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 # Archivo principal para la API generada con Flask.
 from flask import Flask
 
+sonar.python.version=2.7, 3.7, 3.8, 3.9
+
 app = Flask(__name__)
 csrf = CSRFProtect()
 csrf.init_app(app)
